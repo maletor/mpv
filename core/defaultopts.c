2086520cfb1687a31ccd5e8b9b82465acc43d3a0
wm4 <wm4@nowhere>
2013-03-31 04:24:53 +0200

    options: add option to prevent decoder audio downmixing
    Also rename --a52drc to --ad-lavc-ac3drc, and add --ad-lavc-o.

--- core/defaultopts.c
+++ core/defaultopts.c
@@ -72,7 +72,6 @@ void set_default_mplayer_options(struct MPOpts *opts)
         .audio_output_channels = 2,
         .audio_output_format = -1,  // AF_FORMAT_UNKNOWN
         .playback_speed = 1.,
-        .drc_level = 1.,
         .movie_aspect = -1.,
         .sub_auto = 1,
         .osd_bar_visible = 1,
@@ -87,6 +86,10 @@ void set_default_mplayer_options(struct MPOpts *opts)
         .lavc_param = {
             .workaround_bugs = 1, // autodetect
             .error_concealment = 3,
+        },
+        .ad_lavc_param = {
+            .ac3drc = 1.,
+            .downmix = 1,
         },
         .input = {
              .key_fifo_size = 7,
